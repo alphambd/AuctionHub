f77ed53a6c3b7e70afe3aa9dd8beeb11f9850257
Alpha Diallo <alphadiallo.mb@gmail.com>
2026-02-18 07:19:19 +0100

    Initial commit: Structure Backend et Frontend

---
+++ auction-engine/src/main/java/com/auction/auction_engine/AuctionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.auction.auction_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionEngineApplication.class, args);
+	}
+
+}
